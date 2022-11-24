125ba570f581c473f6c79d13d127f3c50550ebdb
disnocen <me@fadibarbara.it>
2022-11-24 09:28:25 +0100

    created test for ot module

---
+++ test_ot.py
@@ -0,0 +1,43 @@
+# this test is for the ot module
+
+import pytest
+import ot
+
+# test it with pytest
+# $ pytest -q test_ot.py
+
+def test_length_num_to_bin():
+    a = 1
+    assert len(ot.num_to_bin(a)) == 128
+
+def test_length_string_to_bin():
+    a = 'abc'
+    assert len(ot.string_to_bin(a)) == 128
+
+def test_length_bin_to_string():
+    a = '00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000011000010110001001100011' # 'abc'
+    assert len(ot.bin_to_string(a)) == 3
+def test_length_xor_int():
+    a = 1
+    b = 2
+    assert len(ot.xor(a, b)) == 128
+
+def test_length_xor_string():
+    a = 'abc'
+    b = 'def'
+    assert len(ot.xor(a, b)) == 128
+
+def test_length_xor_int_and_string():
+    a = 1
+    b = 'abc'
+    assert len(ot.xor(a, b)) == 128
+
+def test_length_xor_string_and_int():
+    a = 'abc'
+    b = 1
+    assert len(ot.xor(a, b)) == 128
+
+def this_test_will_fail():
+    a = 1
+    b = 2
+    assert len(ot.xor(a, b)) == 127
